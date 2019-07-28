f12bc3cfdca9f44f2aac9cb129e5bc513d76d5bc
TomekKasprzycki <tomasz.kasprzycki@gmail.com>
2019-07-28 14:43:03 +0200

    3 servlet added vol. 2

--- src/main/java/pl/coderslab/Servlet13.java
+++ src/main/java/pl/coderslab/Servlet13.java
@@ -15,7 +15,8 @@ public class Servlet13 extends HttpServlet {
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         response.setContentType("text/html");
         response.getWriter().append("<p>Todey is: " + LocalDate.now().toString() + "</p>");
-        response.getWriter().append("Request send from IP: " + request.getRemoteAddr() + "</p>");
+        response.getWriter().append("<p>Request send from IP: " + request.getRemoteAddr() + "</p>");
+        response.getWriter().append("<p>Browser: " + request.getHeader("user-agent")+"</p>");
 
     }
 }
